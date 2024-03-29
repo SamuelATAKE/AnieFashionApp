2d36a368004377c39a248929b4b394190bb1a9c2
SamuelATAKE <samuelatake1881@gmail.com>
2024-03-29 09:35:32 +0000

    first-commit

---
+++ src/components/VerifyEmail.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import { trpc } from '@/trpc/client'
+import { Loader2, XCircle } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { buttonVariants } from './ui/button'
+
+interface VerifyEmailProps {
+  token: string
+}
+
+const VerifyEmail = ({ token }: VerifyEmailProps) => {
+  const { data, isLoading, isError } =
+    trpc.auth.verifyEmail.useQuery({
+      token,
+    })
+
+  if (isError) {
+    return (
+      <div className='flex flex-col items-center gap-2'>
+        <XCircle className='h-8 w-8 text-red-600' />
+        <h3 className='font-semibold text-xl'>
+          Il y avait un problème
+        </h3>
+        <p className='text-muted-foreground text-sm'>
+          Ce jeton n'est pas valide ou est peut-être expiré. Veuillez réessayer.
+        </p>
+      </div>
+    )
+  }
+
+  if (data?.success) {
+    return (
+      <div className='flex h-full flex-col items-center justify-center'>
+        <div className='relative mb-4 h-60 w-60 text-muted-foreground'>
+          <Image
+            src='/hippo-email-sent.png'
+            fill
+            alt='the email was sent'
+          />
+        </div>
+
+        <h3 className='font-semibold text-2xl'>
+          Vous êtes prêt !
+        </h3>
+        <p className='text-muted-foreground text-center mt-1'>
+          Merci d'avoir vérifié votre email.
+        </p>
+        <Link
+          className={buttonVariants({ className: 'mt-4' })}
+          href='/sign-in'>
+          Connexion
+        </Link>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div className='flex flex-col items-center gap-2'>
+        <Loader2 className='animate-spin h-8 w-8 text-zinc-300' />
+        <h3 className='font-semibold text-xl'>
+          Vérification...
+        </h3>
+        <p className='text-muted-foreground text-sm'>
+          Cela ne prendra pas longtemps.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default VerifyEmail
---
+++ src/lib/utils.ts
@@ -0,0 +1,70 @@
+import { type ClassValue, clsx } from 'clsx'
+import { Metadata } from 'next'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(
+  price: number | string,
+  options: {
+    currency?: 'FCFA' | 'EUR' | 'GBP' | 'BDT'
+    notation?: Intl.NumberFormatOptions['notation']
+  } = {}
+) {
+  const { currency = 'FCFA', notation = 'compact' } = options
+
+  const numericPrice =
+    typeof price === 'string' ? parseFloat(price) : price
+
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice)
+}
+
+export function constructMetadata({
+  title = 'AnieFashion - le marché de la mode',
+  description = 'AnieFashion est une place de marché pour les conceptions styliste de haute qualité.',
+  image = '/thumbnail.png',
+  icons = '/favicon.ico',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@SamuelATAKE',
+    },
+    icons,
+    metadataBase: new URL('https://digitalhippo.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
+}
